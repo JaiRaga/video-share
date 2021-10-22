cafee2dfcf0c3395caad4e4a54c2c7e2df43f72b
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-22 10:41:56 +0530

    :zap: aligned icons vertically

--- client/src/components/layout/Drawer.js
+++ client/src/components/layout/Drawer.js
@@ -1,7 +1,83 @@
 import React from 'react'
+import {
+	Grid,
+	IconButton,
+	makeStyles,
+	Tooltip,
+	useMediaQuery,
+} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+import WatchLaterIcon from '@material-ui/icons/WatchLater'
+import HistoryIcon from '@material-ui/icons/History'
+import SettingIcon from '@material-ui/icons/Settings'
+import LogoutIcon from '@material-ui/icons/PersonOutline'
+
+const useStyles = makeStyles((theme) => ({
+	drawer: {
+		[theme.breakpoints.only('xs')]: {
+			display: 'none',
+		},
+	},
+	iconname: {
+		cursor: 'pointer',
+		marginBottom: 10,
+	},
+}))
 
 const Drawer = () => {
-	return <div>Drawer</div>
+	const classes = useStyles()
+
+	return (
+		<Grid
+			container
+			item
+			// xs={1}
+			direction='column'
+			// justifyContent='flex-start'
+			// alignItems='flex-start'
+			className={classes.drawer}>
+			<Grid item>
+				<IconButton aria-label='home'>
+					<HomeIcon />
+				</IconButton>
+			</Grid>
+			<Grid item className={classes.iconname}>
+				Home
+			</Grid>
+			<Grid item>
+				<IconButton aria-label='home'>
+					<WatchLaterIcon />
+				</IconButton>
+			</Grid>
+			<Grid item className={classes.iconname}>
+				Saved
+			</Grid>
+			<Grid item>
+				<IconButton aria-label='home'>
+					<HistoryIcon />
+				</IconButton>
+			</Grid>
+			<Grid item className={classes.iconname}>
+				History
+			</Grid>
+			<Grid item>
+				<IconButton aria-label='home'>
+					<SettingIcon />
+				</IconButton>
+			</Grid>
+			<Grid item className={classes.iconname}>
+				Setting
+			</Grid>
+			<Grid item>
+				<IconButton aria-label='home'>
+					<LogoutIcon />
+				</IconButton>
+			</Grid>
+			<Grid item className={classes.iconname}>
+				Logout
+			</Grid>
+		</Grid>
+	)
 }
 
 export default Drawer
