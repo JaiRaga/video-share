e4ed8aa98e08ce65e0731e6b0531da7d05e877cb
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-30 14:58:38 +0530

    :sparkles: contains action types for auth ans videos

---
+++ client/src/redux/actions/types.js
@@ -0,0 +1,18 @@
+// For Auth
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
+export const REGISTER_FAIL = 'REGISTER_FAIL'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+export const LOGOUT = 'LOGOUT'
+export const USER_LOADED = 'USER_LOADED'
+export const GET_PROFILE = 'GET_PROFILE'
+export const CLEAR_PROFILE = 'CLEAR_PROFILE'
+export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+export const PROFILE_ERROR = 'PROFILE_ERROR'
+export const AUTH_ERROR = 'AUTH_ERROR'
+// For Videos
+export const GET_VIDEOS = 'GET_VIDEOS'
+export const POST_VIDEO = 'POST_VIDEO'
+export const GET_VIDEO = 'GET_VIDEO'
+export const UPDATE_VIDEO = 'UPDATE_VIDEO'
+export const DELETE_VIDEO = 'DELETE_VIDEO'
