810a38a57323a8cc7e861f4138fa5130df62a35e
JaiRaga <raga.j.santhosh@gmail.com>
2021-11-04 12:51:54 +0530

    :zap: passed title and description as props to video items

--- client/src/components/video/Videos.js
+++ client/src/components/video/Videos.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 
 import Video from './Video'
-import spacevid from '../../space.mp4'
-import sample from '../../sample.mp4'
 
 const Videos = () => {
 	const val = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	const { videos } = useSelector((state) => state.video)
 	return (
-		<Grid container item xs={12} justifyContent='flex-start'>
-			{val.map((_, ind) => (
-				// <Video src={ind % 2 === 0 ? spacevid : sample} />
-				<Video key={ind} ind={ind} src={spacevid} />
-			))}
+		<Grid container item xs={12} justifyContent='center'>
+			{videos.length === 0 && <p>Loading...</p>}
+			{videos.length !== 0 &&
+				videos.map(({ _id, owner, secure_url, title, createdAt }) => (
+					<Video
+						key={_id}
+						_id={_id}
+						owner={owner}
+						secureUrl={secure_url}
+						title={title}
+						date={createdAt}
+					/>
+					// <Video key={ind} ind={ind} src={spacevid} />
+				))}
 		</Grid>
 	)
 }
