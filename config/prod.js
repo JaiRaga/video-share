9ed465f4b54c62008ed7bb8b15acc3caa43b5991
JaiRaga <raga.j.santhosh@gmail.com>
2021-11-04 12:46:11 +0530

    :wrench: updated

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  tokenSecret: process.env.TOKEN_SECRET
-};
+	mongoURI: process.env.MONGO_URI,
+	tokenSecret: process.env.TOKEN_SECRET,
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.API_KEY,
+	api_secret: process.env.API_SECRET,
+}
