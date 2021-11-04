788d86751a552c6e85e34428c495de408dd88f46
JaiRaga <raga.j.santhosh@gmail.com>
2021-11-04 12:54:24 +0530

    :sparkles: displays individual video with title and description

---
+++ client/src/components/video/VideoPage.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Grid, makeStyles, Typography } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import offlineStore from '../../utils/offlineStore'
+import Video from './Video'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		padding: '1rem',
+		// backgroundColor: '#999',
+	},
+	videoDisplay: {
+		// backgroundColor: '#666',
+	},
+	videoPlayer: {
+		marginTop: 10,
+		width: '100%',
+		height: '550px',
+	},
+}))
+
+const VideoPage = (props) => {
+	// localforage
+	const offlineState = offlineStore
+	console.log('video page', offlineState)
+
+	const classes = useStyles()
+	// console.log(props)
+	// console.log(props.match.params.id)
+	const videoId = props.match.params.id
+	const { videos } = useSelector((state) => state.video)
+	const video = videos.filter((video) => video._id === videoId)
+	// console.log(video)
+	const { secure_url, title, description } = video[0]
+	// console.log(secure_url)
+	return (
+		<Grid container className={classes.root}>
+			<Grid
+				container
+				item
+				xs={12}
+				direction='column'
+				// alignItems='center'
+				className={classes.videoDisplay}>
+				<video controls autoPlay className={classes.videoPlayer}>
+					<source src={secure_url} type='video/mp4' />
+					Sorry, your browser doesn't support embedded videos.
+				</video>
+				<Typography variant='h4' className={classes.title}>
+					{title}
+				</Typography>
+				<Typography variant='body1' className={classes.title}>
+					{description}
+				</Typography>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default VideoPage
